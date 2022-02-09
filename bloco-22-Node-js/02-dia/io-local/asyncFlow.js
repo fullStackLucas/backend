e2673a7ce1e71e60d8ac347393064cef61200c6f
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-02-09 18:46:56 -0300

    test: adding tests for functi for asyncFlow.js

---
+++ bloco-22-Node-js/02-dia/io-local/asyncFlow.js
@@ -0,0 +1,21 @@
+const calculateNumbers = async (num1, num2, num3) => {
+  const promise = await new Promise((resolve, reject) => {
+    const newArray = [num1, num2, num3];
+    const areArgumentsValid = newArray.every((number) => typeof(number) === 'number')
+    const calculateArguments = (num1 + num2) * num3
+    if(!areArgumentsValid) reject('Informe apenas números');
+    if(calculateArguments < 50) reject('Valor muito baixo');
+    try {
+      resolve(calculateArguments)
+    } catch (error) {
+      console.log(error.message);
+    } 
+  })
+  return promise;
+};
+
+calculateNumbers(2, 4, 5)
+  .then((response) => console.log(response))
+  .catch((error) => console.log(error))
+
+module.exports = calculateNumbers;
