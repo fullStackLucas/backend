bef4aafc802f1b4ab9d35adde57a447dfa7370b2
Lucas Pinheiro <86744794+fullStackLucas@users.noreply.github.com>
2022-02-09 20:04:46 -0300

    Update asyncFlow.js

--- bloco-22-Node-js/02-dia/io-local/asyncFlow.js
+++ bloco-22-Node-js/02-dia/io-local/asyncFlow.js
@@ -1,12 +1,12 @@
 const calculateNumbers = async (num1, num2, num3) => {
   const promise = await new Promise((resolve, reject) => {
     const newArray = [num1, num2, num3];
-    const areArgumentsValid = newArray.every((number) => typeof(number) === 'number')
-    const calculateArguments = (num1 + num2) * num3
+    const areArgumentsValid = newArray.every((number) => typeof(number) === 'number');
+    const calculateArguments = (num1 + num2) * num3;
     if(!areArgumentsValid) reject('Informe apenas números');
     if(calculateArguments < 50) reject('Valor muito baixo');
     try {
-      resolve(calculateArguments)
+      resolve(calculateArguments);
     } catch (error) {
       console.log(error.message);
     } 
@@ -16,6 +16,6 @@ const calculateNumbers = async (num1, num2, num3) => {
 
 calculateNumbers(2, 4, 5)
   .then((response) => console.log(response))
-  .catch((error) => console.log(error))
+  .catch((error) => console.log(error));
 
 module.exports = calculateNumbers;
