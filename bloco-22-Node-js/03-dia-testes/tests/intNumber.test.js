678517881c8a8925703ef4e93f89a35a23fb877d
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-02-10 15:49:34 -0300

    fix: fixing error messages from intNumber.test.js

--- bloco-22-Node-js/03-dia-testes/tests/intNumber.test.js
+++ bloco-22-Node-js/03-dia-testes/tests/intNumber.test.js
@@ -3,23 +3,23 @@ const intNumber = require('../integerNumber')
 
 describe.only('Should return a string if a number is provided, telling what type of int this number is', () => {
   it('should throw an error if a number is not provided', () => {
-    expect(() => intNumber('hello').to.throw(new Error('A number must be provided')));
+    expect(() => intNumber('number')).to.throw(/A number must be provided/i);
   })
 
   it('should throw an error if a number is not provided', () => {
-    expect(() => intNumber('0').to.throw(new Error('A number must be provided')));
+    expect(() => intNumber('0')).to.throw(/A number must be provided/i);
   })
 
   it('should throw an error if a number is not provided', () => {
-    expect(() => intNumber('1').to.throw(new Error('A number must be provided')));
+    expect(() => intNumber('-1')).to.throw(/A number must be provided/i);
   })
 
   it('should throw an error if a number is not provided', () => {
-    expect(() => intNumber('-1').to.throw(new Error('A number must be provided')));
+    expect(() => intNumber('1')).to.throw(/A number must be provided/i);
   })
 
   it('should throw an error if a number is not provided', () => {
-    expect(() => intNumber().to.throw(new Error('A number must be provided')));
+    expect(() => intNumber()).to.throw(/A number must be provided/i);
   })
 
   it('should return a string if a number is provided', () => {
