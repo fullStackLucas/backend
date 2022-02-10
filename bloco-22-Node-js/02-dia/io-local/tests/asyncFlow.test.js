510c542ec31777f68d239102f47ebacc1f255190
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-02-10 20:02:45 -0300

    docs: including last day features and docs

--- bloco-22-Node-js/02-dia/io-local/tests/asyncFlow.test.js
+++ bloco-22-Node-js/02-dia/io-local/tests/asyncFlow.test.js
@@ -1,4 +1,4 @@
-const calculateNumbers = require('./asyncFlow');
+const calculateNumbers = require('../asyncFlow');
 
 describe('testing possible returns from Promises of CalculateNumbers function', () => {
   it('should return the response of the Promise', async () => {
@@ -12,6 +12,15 @@ describe('testing possible returns from Promises of CalculateNumbers function', 
       await calculateNumbers(1, 3, 5);
     } catch (e) {
       expect(e).toMatch(/Valor muito baixo/i);
+    }
+  });
+
+  it('the function fails with an error if any parameters are invalid', async () => {
+    expect.assertions(1);
+    try {
+      await calculateNumbers('1', 3, 5);
+    } catch (e) {
+      expect(e).toMatch(/Informe apenas números/i);
     }
   });
 
