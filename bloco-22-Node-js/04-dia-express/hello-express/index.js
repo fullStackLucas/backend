8c9dcfce8bcb8af0d557d298b2d12ad4d1ac0163
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-02-11 14:02:54 -0300

    feat: creating endpoints to recipes and drinks sorted by name

--- bloco-22-Node-js/04-dia-express/hello-express/index.js
+++ bloco-22-Node-js/04-dia-express/hello-express/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const recipes = [
@@ -7,8 +8,31 @@ const recipes = [
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
-app.get('/recipes', function (req, res) {
-  res.json(recipes);
+const drinks = [
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
+
+function compare(a, b) {
+  if (a.name < b.name) return -1;
+  if (a.name > b.name) return 1;
+  return 0;
+}
+
+app.use(cors());
+
+app.get('/recipes', (_req, res) => {
+  const orderedRecipes = recipes.sort(compare); 
+  res.json(orderedRecipes);
+});
+
+app.get('/drinks', (_req, res) => {
+  const orderedDrinks = drinks.sort(compare);
+  res.json(orderedDrinks);
 });
 
 app.listen(3001, () => {
